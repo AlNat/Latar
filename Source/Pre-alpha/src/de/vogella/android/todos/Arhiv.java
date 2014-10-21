421a72012543dbf364d415b47faee5b1fb944e08
DKrus <dk@morfi.us>
2014-10-21 23:21:00 +0400

    Pre-alpha

---
+++ Source/Pre-alpha/src/de/vogella/android/todos/Arhiv.java
@@ -0,0 +1,14 @@
+package de.vogella.android.todos;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class Arhiv extends Activity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.arh);
+  }
+}
---
+++ Source/Pre-alpha/src/de/vogella/android/todos/database/TodoDatabaseHelper.java
@@ -0,0 +1,37 @@
+package de.vogella.android.todos.database;
+ 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+ 
+public class TodoDatabaseHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "applicationdata";
+ 
+    private static final int DATABASE_VERSION = 1;
+ 
+    // запрос на создание базы данных
+    private static final String DATABASE_CREATE = "create table todo (_id integer primary key autoincrement, "
+            + "category text not null, summary text not null, description text not null);";
+ 
+    public TodoDatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+ 
+    // метод вызывается при создании базы данных
+    @Override
+    public void onCreate(SQLiteDatabase database) {
+        database.execSQL(DATABASE_CREATE);
+    }
+ 
+    // метод вызывается при обновлении базы данных, например, когда вы увеличиваете номер версии базы данных
+    @Override
+    public void onUpgrade(SQLiteDatabase database, int oldVersion,
+            int newVersion) {
+        Log.w(TodoDatabaseHelper.class.getName(),
+                "Upgrading database from version " + oldVersion + " to "
+                        + newVersion + ", which will destroy all old data");
+        database.execSQL("DROP TABLE IF EXISTS todo");
+        onCreate(database);
+    }
+}
